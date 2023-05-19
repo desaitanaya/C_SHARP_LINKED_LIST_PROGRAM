316aec99a6738ff15b45410aeaa58767571d8e2b
Tanaya <desaitanaya17@gmail.com>
2023-05-19 17:13:08 +0530

    [Tanaya] Add : C# Program for inserting a new node at the start of the Linked list.

--- LinkedListProgram/Program.cs
+++ LinkedListProgram/Program.cs
@@ -1,10 +1,64 @@
-﻿namespace LinkedListProgram
+﻿namespace LinkedList
 {
+    //Creating a Node in Linked list
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node head;
+
+        public Node(int x)
+        {
+            data = x;
+            next = null;
+        }
+
+        public Node()
+        {
+        }
+
+        //Method for inserting value at start of the linked list
+        public void InsertNodeStart(int data)
+        {
+            Node newNode = new Node(data);
+            //Checking if the linked list is empty
+            if (head == null)
+            {
+                head = newNode;
+            }
+            //Inserting a new node at the start
+            else
+            {
+                newNode.next = head;
+                head = newNode;
+            }
+
+        }
+
+        //Method for displaying the linked list
+        public void DisplayNode()
+        {
+            Node temp = head;
+            while (temp != null)
+            {
+                Console.Write(temp.data + "->");
+                temp = temp.next;
+            }
+
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Node node = new Node();
+
+            node.InsertNodeStart(10);
+            node.InsertNodeStart(20);
+
+            node.DisplayNode();
+
         }
     }
 }
