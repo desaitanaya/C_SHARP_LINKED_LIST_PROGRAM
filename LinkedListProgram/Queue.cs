6f7aee3aae9874bc8a3abdbce61779b53f354d29
Tanaya <desaitanaya17@gmail.com>
2023-05-20 20:20:22 +0530

    [Tanaya] Add : C# Program to perform Queue operations using Linked list.

---
+++ LinkedListProgram/Queue.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProgram
+{
+    class Queue<T> where T : IComparable<T>
+    {
+        Node<T> node = new Node<T>();
+        public void Enqueue(T data)
+        {
+            node.InsertNodeEnd(data);
+        }
+
+        public void Dequeue()
+        {
+            node.DeleteNodeStart();
+        }
+
+        public void DisplayStack()
+        {
+            node.DisplayNode();
+        }
+    }
+
+    class QueueProgram<T>
+    {
+        static void main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(10);
+            queue.DisplayStack();
+
+            queue.Enqueue(20);
+            queue.DisplayStack();
+
+            queue.Enqueue(30);
+            queue.DisplayStack();
+
+            queue.Dequeue();
+            queue.DisplayStack();
+
+
+        }
+    }
+}
